640499e7883d3525c91fbc4768a5f242420411a0
avanim <axm1205@case.edu>
2022-12-07 17:00:12 -0500

    edited HTML and styling, added employee icons, email and github links

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -5,33 +5,42 @@ function generateCards(teamRoster) {
         const {name, id, email, _} = employee;
         let uniqueProp;
         let uniqueValue;
+        let iconClass;
         // Depending on which type current employee is, 
         // set uniqueProp and uniqueValue as last prop/value in their constructor
         if (employeeType === "Manager") {
             uniqueProp = "Office Number";
             uniqueValue = employee.getOfficeNumber();
+            iconClass = "fa-solid fa-people-roof";
         } else if (employeeType === "Engineer") {
-            uniqueProp = "Github";
-            uniqueValue = employee.getGithub();
+            uniqueProp = "GitHub";
+            //uniqueValue = employee.getGithub();
+            uniqueValue = `<a href="https://github.com/${employee.getGithub()}" target="_blank"
+            style="text-decoration: none;">${employee.getGithub()}</a>`;
+            iconClass = "fa-solid fa-laptop-code";
         } else if (employeeType === "Intern") {
             uniqueProp = "School";
             uniqueValue = employee.getSchool();
+            iconClass = "fa-solid fa-graduation-cap";
         }
         
         // Return HTML to generate card for that employee
-        return `<div class="card shadow" style="width: 18rem;">
-    <div class="card-header bg-primary text-white">
-        <h3>${name}</h3>
-        <h4>${employeeType}</h4>
+        return `<div class="col d-flex justify-content-center mt-4">
+    <div class="card shadow "style="width: 20rem;">
+        <div class="card-header bg-primary text-white">
+            <h3 class="fs-2">${name}</h3>
+            <h4><i class="${iconClass}"></i> ${employeeType}</h4>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group py-3">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${email}"
+                        style="text-decoration: none;">${email}</a></li>
+                <li class="list-group-item">${uniqueProp}: ${uniqueValue}</li>
+            </ul>
+        </div>
     </div>
-    <div class="card-body bg-light">
-        <ul class="list-group">
-            <li class="list-group-item">ID: ${id}</li>
-            <li class="list-group-item">Email: ${email}</li>
-            <li class="list-group-item">${uniqueProp}: ${uniqueValue}</li>
-        </ul>
-    </div>
-</div>`;
+</div>`
     });
 
     // Return result of joining each employee card HTML by a new line 
@@ -41,6 +50,7 @@ function generateCards(teamRoster) {
 function generateHTML(teamRoster) {
     return `<!DOCTYPE html>
     <html lang="en">
+    
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -50,15 +60,21 @@ function generateHTML(teamRoster) {
     </head>
     
     <body>
-        <header class="bg-danger text-white text-center p-5">
-            <h1>My Team</h1>
-        </header>    
+        <header class="bg-danger text-white text-center p-4">
+            <h1 class="fw-bolder" style="font-size: 3rem;">My Team</h1>
+        </header>
     
         <main class="d-flex justify-content-center align-items-center">
+            
+            <section class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 d-flex flex-wrap justify-content-center align-items-center w-75">
             ${generateCards(teamRoster)}
-        </main>
-    </body>
-    </html>`;
+            </section>
+        
+    </main>
+    <script src="https://kit.fontawesome.com/9f96f948a1.js" crossorigin="anonymous"></script>
+</body>
+
+</html>`;
 }
 
 module.exports = generateHTML;
