26e0f5d0231f8b082692eda8f4db5062c8c29fdb
avanim <axm1205@case.edu>
2022-12-07 23:38:01 -0500

    added tests to Engineer.test.js

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,50 @@
+const Engineer = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+    describe("Initialization", () => {
+        it("should create an object with 'name', 'id', 'email', 'github' properties set to the 'name', 'id', 'email', 'github' arguments provided when called with the 'new' keyword", () => {
+            // Arrange
+            const name = "Jane";
+            const id = 5;
+            const email = "jane@test.edu";
+            const github = "jane123";
+    
+            // Act
+            const obj = new Engineer(name, id, email, github);
+    
+            // Assert
+            expect(obj.name).toEqual(name);
+            expect(obj.id).toEqual(id);
+            expect(obj.email).toEqual(email);
+            expect(obj.github).toEqual(github);
+        });
+    });
+
+    describe("Methods", () => {
+        it("getName(), getId(), getEmail(), getGithub() should return object's 'name', 'id', 'email', 'github' properties respectively", () => {
+            // Arrange
+            const name = "Jane";
+            const id = 5;
+            const email = "jane@test.edu";
+            const github = "jane123";
+
+            // Act
+            const obj = new Engineer(name, id, email, github);
+
+            // Assert
+            expect(obj.getName()).toEqual(obj.name);
+            expect(obj.getId()).toEqual(obj.id);
+            expect(obj.getEmail()).toEqual(obj.email);
+            expect(obj.getGithub()).toEqual(obj.github);
+        });
+
+        it("getRole() should return a string representation of the type of object", () => {
+            // Act
+            const obj = new Engineer("Jane", 5, "jane@test.edu", "jane123");
+
+            // Assert
+            expect(typeof obj.getRole()).toEqual("string");
+            expect(obj.getRole()).toEqual(obj.constructor.name);
+        });
+    });
+});
